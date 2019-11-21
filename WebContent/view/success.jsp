39b4e88263eb901652e4b64a483d726e0cffa924
jadilovic <adilovic79@yahoo.com>
2019-11-21 23:51:09 +0100

    final commit

--- WebContent/view/success.jsp
+++ WebContent/view/success.jsp
@@ -11,7 +11,7 @@
 <body>
 
 	<%
-		User user = (User) request.getAttribute("user");
+		User user = (User) session.getAttribute("user");
 	%>
 
 	<h1>Login successful</h1>
--- src/controller/FilterLoginServlet.java
+++ src/controller/FilterLoginServlet.java
@@ -13,7 +13,7 @@ import model.LoginBO;
 import model.User;
 
 @WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+public class FilterLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -35,18 +35,29 @@ public class LoginServlet extends HttpServlet {
 			User user = new User();
 			user.setUsername(username);
 			user.setPassword(password);
-			
-			HttpSession session = request.getSession();
 
+			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
+			
+			System.out.println("User " + user.getUsername() + " has been logged in");
 
 			RequestDispatcher success = request.getRequestDispatcher("view/success.jsp");
 			success.forward(request, response);
 
 		} else {
+			
+			String error = "Invalid user, plase try again";
+			request.setAttribute("error", error);
+			
 			RequestDispatcher failure = request.getRequestDispatcher("view/login.jsp");
 			failure.forward(request, response);
 		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
 	}
 
 }
